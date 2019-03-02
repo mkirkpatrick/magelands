a8d8725258f262a9f75bf843a2bca48c6c913ed9
mkirkpatrick <mtraviskirkpatrick@gmail.com>
2019-03-01 23:06:25 -0600

    MeshBuilder Utility created

--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -22,7 +22,7 @@ public class GameController : MonoBehaviour {
 
         landController.lands = new Land[2];
         landController.lands[0] = new Land(new Vector3(0, 0, 0), 10, 20);
-        landController.lands[1] = new Land(new Vector3(0, 0, 22), 20, 20);
+        landController.lands[1] = new Land(new Vector3(0, 0, 32), 32, 32);
 
     }
     void Start() {
---
+++ Assets/Scripts/Utility/MeshBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MeshBuilder
+{
+
+    public Vector3[] BuildGroundMesh() {
+    }
+
+
+}

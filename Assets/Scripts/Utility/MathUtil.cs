318bc6d94eade264e4f98e833fc4ba99fd6a9c4a
mkirkpatrick <mtraviskirkpatrick@gmail.com>
2019-04-23 19:08:43 -0500

    Paths started. Bezier Curve Math.

---
+++ Assets/Scripts/Utility/MathUtil.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathUtil
+{
+    public static Vector2 CalculateCubicBezierPoint(float t, Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3)
+    {
+        float u = 1 - t;
+        float tt = t * t;
+        float uu = u * u;
+        float uuu = uu * u;
+        float ttt = tt * t;
+
+        Vector2 p = uuu * p0;
+        p += 3 * uu * t * p1;
+        p += 3 * u * tt * p2;
+        p += ttt * p3;
+
+        return p;
+    }
+}
